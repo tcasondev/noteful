2a55381d668127bd727a637fdb485a3d5c4b3f02
Tanner Cason <tcasondevv@gmail.com>
2021-02-12 10:30:30 -0600

    troubleshooting update

--- src/NotefulApp/NotefulApp.js
+++ src/NotefulApp/NotefulApp.js
@@ -18,7 +18,7 @@ class NotefulApp extends React.Component {
     }
 
     getFolderData() {
-        fetch(`http://noteful-tanner-cason.herokuapp.com/folders`)
+        fetch(`http://noteful-tanner-cason.herokuapp.com/api/folder`)
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Hey, something went wrong')
@@ -36,7 +36,7 @@ class NotefulApp extends React.Component {
     }
 
     getNoteData() {
-        fetch(`http://localhost:9090/notes`)
+        fetch(`http://noteful-tanner-cason.herokuapp.com/api/note`)
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Hey, something went wrong')
